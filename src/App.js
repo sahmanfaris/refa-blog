6adbd79be3b3fc0e35df70dba1292ff19c2b0d2d
unknown <farissahman@yahoo.com>
2021-02-25 13:59:03 +0100

    blog details

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import BlogDetails from './BlogDetails'
+import Create from './Create'
 import Home from './Home'
 import Navbar from './Navbar'
 
 function App() {
   return (
-    <div className='App'>
-      <Navbar />
-      <Home />
-    </div>
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <div className='content'>
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route path='/create'>
+              <Create />
+            </Route>
+            <Route path='/blogs/:id'>
+              <BlogDetails />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   )
 }
 
--- src/useFetch.js
+++ src/useFetch.js
@@ -6,7 +6,8 @@ const useFetch = url => {
   const [error, setError] = useState(null)
 
   useEffect(() => {
-    fetch(url)
+    const abortCont = new AbortController()
+    fetch(url, { signal: abortCont.signal })
       .then(res => {
         if (!res.ok) {
           throw Error('could not fetch the data for that resourse')
@@ -19,9 +20,14 @@ const useFetch = url => {
         setError(null)
       })
       .catch(err => {
-        setIsLoading(false)
-        setError(err.message)
+        if (err.name === 'AbortError') {
+          console.log('fetch aborted')
+        } else {
+          setIsLoading(false)
+          setError(err.message)
+        }
       })
+    return () => abortCont.abort()
   }, [url])
 
   return { data, isLoading, error }
