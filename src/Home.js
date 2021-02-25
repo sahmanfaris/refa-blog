1957dc76ee7ebe6897912c004e96267ed9f46bdc
unknown <farissahman@yahoo.com>
2021-02-25 09:00:18 +0100

    bloglist

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,24 @@
+import { useState } from 'react'
+import BlogList from './BlogList'
+
 const Home = () => {
-  const handleClick = e => {
-    console.log('hello ninjas', e)
-  }
-
-  const handleClickAgain = (name, e) => {
-    console.log('hello ' + name, e.target)
-  }
-
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'faris', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    {
+      title: 'Web dev top tips',
+      body: 'lorem ipsum...',
+      author: 'faris',
+      id: 3,
+    },
+  ])
   return (
     <div className='home'>
-      <h2>Homepage</h2>
-      <button onClick={handleClick}>Click me</button>
-      <button onClick={e => handleClickAgain('mario', e)}>
-        Click me again
-      </button>
+      <BlogList blogs={blogs} title='All Blogs' />
+      <BlogList
+        blogs={blogs.filter(blog => blog.author === 'faris')}
+        title='Faris Blogs'
+      />
     </div>
   )
 }
